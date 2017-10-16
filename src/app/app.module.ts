6dd6763d8e341a69fca2599e64999223ce5d517b
Alex Batlin <alex@batlin.com>
2017-10-16 15:11:40 +0100

    adding angular capabilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { MatToolbarModule, MatIconModule, MatButtonModule } from '@angular/material';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
