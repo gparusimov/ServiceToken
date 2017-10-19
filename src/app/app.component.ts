0c864889eaf4857ad6a4941e6930c43505d36634
Alex Batlin <alex@batlin.com>
2017-10-19 11:19:57 +0100

    quick check-in ref #3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { Web3Service } from "./web3/web3.service";
 import { MatSnackBar } from '@angular/material';
 
+export class Agreement {
+  constructor(
+    public tokenName: string,
+    public tokenSymbol: string,
+    public decimalPlaces: number,
+    public totalSupply: number,
+    public validFromDate: Date,
+    public validFromHour: number,
+    public validFromMinute: number,
+    public expiresEndDate: Date,
+    public expiresEndHour: number,
+    public expiresEndMinute: number,
+    public issuerAddress: string,
+    public beneficiaryAddress: string
+  ) { }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,6 +30,7 @@ export class AppComponent implements OnInit {
   accounts : string[];
   account: string;
   Factory : Promise<any>;
+  agreement: Agreement;
 
   constructor(private web3Service : Web3Service, public snackBar: MatSnackBar) {
     console.log("AccountComponent constructor: " + web3Service);
@@ -20,6 +38,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.watchAccount();
+
+    this.newAgreement();
 
     this.Factory = new Promise((resolve, reject) => {
       setInterval(() => {
@@ -30,31 +50,41 @@ export class AppComponent implements OnInit {
     });
   }
 
-  watchAccount() {
-    this.web3Service.accountsObservable.subscribe((accounts) => {
-      this.accounts = accounts;
-      this.account = accounts[0];
-    });
-  }
-
-  create() {
-    console.log("Creating");
+  onSubmit() {
+    console.log("submitting");
 
     this.Factory.then((contract) => {
       return contract.deployed();
     }).then((factoryInstance) => {
-      return factoryInstance.createAgreement.sendTransaction("FlexiTimeToken", "FTT", 0, 240, 1508252290, 1508252290, this.account, this.account, {from: this.account});
+      return factoryInstance.createAgreement.sendTransaction(
+        this.agreement.tokenName,
+        this.agreement.tokenSymbol,
+        this.agreement.decimalPlaces,
+        this.agreement.totalSupply,
+        this.toEpoch(this.agreement.validFromDate, this.agreement.validFromHour, this.agreement.validFromMinute),
+        this.toEpoch(this.agreement.expiresEndDate, this.agreement.expiresEndHour, this.agreement.expiresEndMinute),
+        this.account,
+        this.account,
+        {from: this.account}
+      );
     }).then((success) => {
       if (!success) {
         this.openSnackBar("Transaction failed!", "Dismiss");
       }
       else {
+        this.newAgreement();
         this.openSnackBar("Transaction complete!", "Dismiss");
       }
     }).catch((e) => {
       this.openSnackBar("Error creating agreement; see log.", "Dismiss");
       console.log(e);
-      
+    });
+  }
+
+  watchAccount() {
+    this.web3Service.accountsObservable.subscribe((accounts) => {
+      this.accounts = accounts;
+      this.account = accounts[0];
     });
   }
 
@@ -77,5 +107,21 @@ export class AppComponent implements OnInit {
     this.snackBar.open(message, action, {
       duration: 2000,
     });
+  }
+
+  newAgreement() {
+    this.agreement = new Agreement(null, null, null, null, new Date(), 0, 0, new Date(), 0, 0, this.account, null);
+  }
+
+  toEpoch(date: Date, hour: number, minute: number) {
+    return Math.round(date.getTime() / 1000) + hour * 60 * 60 + minute * 60;
+  }
+
+  tester1() {
+    return this.toEpoch(this.agreement.validFromDate, this.agreement.validFromHour, this.agreement.validFromMinute);
+  }
+
+  tester2() {
+    return this.toEpoch(this.agreement.expiresEndDate, this.agreement.expiresEndHour, this.agreement.expiresEndMinute);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule, MatIconModule, MatButtonModule, MatTooltipModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCardModule, MatSnackBarModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { Web3Module } from './web3/web3.module';
 
@@ -12,6 +13,7 @@ import { Web3Module } from './web3/web3.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
