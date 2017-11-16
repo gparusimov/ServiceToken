d7209b7ebf43731f4efae92a2d0ad971e5202346
Richard Ireland <richard@reefsolutions.co.uk>
2017-11-16 10:10:33 +0000

    xx

--- truffle.js
+++ truffle.js
@@ -1,16 +1,15 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "couch solve unique spirit wine fine occur rhythm foot feature glory away";
+var mnemonic = "MNEMONIC";
 
 module.exports = {
   networks: {
     development: {
-      host: "35.177.57.188",
-      //host: "localhost",
+      host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
     },
     rinkeby: {
-      provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/PaJsiik099LNct8ZKMlH"), // This will be replaced at runtime with the key, held by AWS
+      provider: new HDWalletProvider(mnemonic, "RINKEBY_PROVIDER_URL"), 
       network_id: '4' // Official for rinkeby (3 for ropsten) 
     }
   }
